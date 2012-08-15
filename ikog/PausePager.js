13a35efeca6f6d18f9b66770b13f827ee7840564
Amit Upadhyay <comments@amitu.com>
2012-08-16 03:24:41 +0530

    minor

--- ikog/PausePager.js
+++ ikog/PausePager.js
@@ -8,12 +8,8 @@ define(["dojo/_base/declare", "ikog/Pager"], function(declare, Pager) {
 			{
 				if (lines[i] == this.PAUSE) break;
 			}
-			console.log("print_once");
-			console.log("remaining_lines", this.remaining_lines);
 			this.remaining_lines = lines.slice(i+1);
-			console.log("remaining_lines", this.remaining_lines);
 			var lines_to_display = lines.slice(0, i);
-			console.log("lines_to_display", lines_to_display);
 			for (i=0; i < lines_to_display.length; i++)
 			{
 				this.ikog.println(lines_to_display[i]);
