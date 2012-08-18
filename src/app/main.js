bdef7b4bd04f779deed5401bcb98220788314c5d
Amit Upadhyay <comments@amitu.com>
2012-08-19 02:40:21 +0530

    some more command processing

--- src/app/main.js
+++ src/app/main.js
@@ -13,6 +13,8 @@ define(
 		var ikog = {
 			MAGIC_TAG: "#!<^",
 			ENCRYPTION_MARKER: "{}--xx",
+			print_current: true,
+			review: false,
 			init: function() {
 				parser.parse();
 				Parse.initialize(
@@ -21,6 +23,7 @@ define(
 	            );
 				ikog.$inp.focus().on_enter(function(line){
 					ikog.process_line(line);
+					ikog.print_current_if_required();
 				}, true);
 				query("body").on("click", function(){ ikog.$inp.focus(); });
 				ikog.print_banner();
@@ -33,12 +36,25 @@ define(
 				);
 				win.scrollIntoView("log_msg_" + ikog.log_id);
 			},
+			print_line: function() {
+				this.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			},
+			print_current_if_required: function(){
+				if (!this.print_current) return;
+				this.print_line();
+				this.println("[00] send mail to coupan owners");
+				this.println("Priority: 05")
+				this.println("Context:  @Anywhere")
+				this.println("Created: [2009-03-10]")
+				this.print_line();				
+			},
 			clear_screen: function() {
 				dc.empty("log");
 			},
 			print_banner: function () {
 				ikog.println(bannertxt);
 				ikog.println("Enter HELP for instructions.");	      
+				this.print_current_if_required();
 			},
 			page: function(lines) {
 				ikog.pager = new Pager(lines);
@@ -48,7 +64,7 @@ define(
 				ikog.pager = new PausePager(helptxt.split("\n"));
 			},
 			parse_input: function(line) {
-				var cmd = "", rest = "";
+				var cmd = "", rest = "", orig = line;
 				if (line.indexOf(this.MAGIC_TAG) == 0)
 	                this.print_error(
 						"You cannot begin lines with the sequence " + this.MAGIC_TAG
@@ -68,20 +84,39 @@ define(
 	                    rest = "";
 					}
 				}
-		        return {cmd: cmd, rest: rest};
+				// TODO: handle shortcuts
+		        return {cmd: cmd, rest: rest, orig: orig};
 			},
+			next_task: function() {}, // TODO
 			process_line: function(line) {
-				
 				console.log("process_line", line, this.pager);
-				if (this.pager) return this.pager.process_line(line);
+				this.print_current = true;
+				if (this.pager) {
+					this.print_current = false;
+					return this.pager.process_line(line);
+				}
+
+				ikog.println(">>> " + line);
 				
 				line = this.parse_input(line);
-				console.log(line);
-				ikog.println(line);
 				
-				if (line == "?") return this.println(quicktxt);
-				if (line == "cls") return this.clear_screen();
-				if (line == "help") return this.show_help();
+				var cmd = line.cmd.toUpperCase();
+				
+				if (cmd == "") {
+					if (this.review) {
+						this.next_task();
+					}
+				}
+				else if (cmd == "?") {
+					this.print_current = false;
+					return this.println(quicktxt);
+				}
+				else if (cmd == "CLS") return this.clear_screen();
+				else if (cmd == "HELP") {
+					this.print_current = false;
+					return this.show_help();
+				}
+				
 			}
 		}
 		ikog.$inp = query("#inp");
