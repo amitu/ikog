01f17cd9e9ba93c28bc7ce1de4bf83a0fee2795a
Amit Upadhyay <comments@amitu.com>
2012-09-10 15:33:40 +0530

    saving history to localstore

--- src/amitu/CommandLineWithHistory.js
+++ src/amitu/CommandLineWithHistory.js
@@ -5,9 +5,16 @@ define(
             history: null,
             index: 0,
             current_line: "",
+            save_to: "amitu_clwh", // set it to "dont" to disable saving
+            _setSave_toAttr: function(value) {
+                this.save_to = value;
+            },
             postCreate: function() {
                 this.inherited(arguments);
-                this.history = [];
+                if (this.save_to != "dont") {
+                    this.history = $.jStorage.get(this.save_to);
+                    if (!this.history) this.history = [];
+                }                    
                 var widget = this;
                 widget.$domNode.on("keyup", function(evt){
                     var key = evt.charCode || evt.keyCode || 0;
@@ -18,13 +25,17 @@ define(
                 this.on("command", function(cmd) {
                     if (cmd != "" && this.history[this.history.length-1] != cmd)
                         this.history.push(cmd);
+                        if(this.save_to != "dont") 
+                            $.jStorage.set(this.save_to, this.history);
                 });
             },
             goup: function() {
                 if (this.index >= this.history.length) return;
                 if (this.index == 0) this.current_line = this.$domNode.val();
                 this.index += 1;
-                this.$domNode.val(this.history[this.history.length - this.index]);
+                this.$domNode.val(
+                    this.history[this.history.length - this.index]
+                );
             },
             godown: function() {
                 if (this.index == 0) return;
